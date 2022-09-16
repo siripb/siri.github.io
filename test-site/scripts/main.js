c3704b394a162230ddcef81294bac2822efe3d88
siripb <88156581+siripb@users.noreply.github.com>
2022-09-16 23:49:00 +0200

    Add files via upload

---
+++ test-site/scripts/main.js
@@ -0,0 +1,17 @@
+const myHeading = document.querySelector('h3');
+myHeading.textContent = 'Hello World'
+
+
+let myVariable = 'Bob'
+myVariable='Steve'
+
+let iceCream = 'chocolate';
+if (iceCream === 'chocolate') {
+    alert('Yay,I love chocolate ice cream!');
+} else {
+    alert('Awww, but chocolate is my favorite...');
+}
+
+document.querySelector('html').addEventListener('click',function () {
+    alert ('Ouch! Stop poking me!');
+});
